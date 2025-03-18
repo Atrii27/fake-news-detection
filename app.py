a98ce1a918089a2b81a7875cd7d29b0db2a75f8b
Atrii27 <162042497+Atrii27@users.noreply.github.com>
2025-03-18 12:46:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
-
 app = Flask(__name__)
-
-# Load Model and Vectorizer
 model = pickle.load(open('../models/logistic_model.pkl', 'rb'))
 tfidf = pickle.load(open('../models/tfidf_vectorizer.pkl', 'rb'))
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     news_text = request.form['news']
@@ -19,6 +14,5 @@ def predict():
     prediction = model.predict(transformed_text)[0]
     result = 'Fake' if prediction == 1 else 'Real'
     return render_template('index.html', prediction=result)
-
 if __name__ == "__main__":
     app.run(debug=True)
