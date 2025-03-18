5f4a07443df849273359501e5e4208ecdbd43164
Atrii27 <162042497+Atrii27@users.noreply.github.com>
2025-03-18 12:47:47 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,11 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
-
-# TF-IDF Vectorization
 tfidf = TfidfVectorizer(max_features=5000)
 X_train_tfidf = tfidf.fit_transform(X_train)
 X_test_tfidf = tfidf.transform(X_test)
-
-# Save vectorizer
 with open('../models/tfidf_vectorizer.pkl', 'wb') as f:
     pickle.dump(tfidf, f)
