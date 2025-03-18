c3172245008fb8a116b07e678b2c820c6279d887
Atrii27 <162042497+Atrii27@users.noreply.github.com>
2025-03-18 12:48:13 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -1,17 +1,9 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import pickle
-
-# Train Logistic Regression Model
 model = LogisticRegression()
 model.fit(X_train_tfidf, y_train)
-
-# Predictions
 y_pred = model.predict(X_test_tfidf)
-
-# Model Evaluation
 print(classification_report(y_test, y_pred))
-
-# Save Model
 with open('../models/logistic_model.pkl', 'wb') as f:
     pickle.dump(model, f)
